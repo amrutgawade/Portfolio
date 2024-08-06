ff09a35bf8ac285b6622a322b21700cdac2cd71a
amrutgawade <amrutgawade.23@gmail.com>
2024-08-07 01:13:41 +0530

    feat: projects updated

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,6 +4,7 @@ import Project3 from "../assets/projects/project-3.png"
 import Project4 from "../assets/projects/project-4.png"
 import Project5 from "../assets/projects/project-5.png"
 import Project6 from "../assets/projects/project-6.png"
+import Project7 from "../assets/projects/project-7.png"
 const projects = [
     {
         id: 1,
@@ -15,41 +16,49 @@ const projects = [
     },
     {
         id: 2,
-        title: "Dattakrupa Jewellers",
+        title: "FreakDevs Official Site",
         category: ["UIUX"],
         image: Project2,
-        live: "https://dattakrupajewellers.in/",
-        github: "https://github.com/FreakDevelopers/dattakrupajewellers"
+        live: "https://freakdevs-official-site.onrender.com/",
+        github: "https://github.com/amrutgawade/FreakDevs-Official-Site/tree/dev"
     },
     {
         id: 3,
-        title: "FreakDevs-Invoice",
-        category: ["UIUX", "MERN"],
+        title: "Dattakrupa Jewellers",
+        category: ["UIUX"],
         image: Project3,
-        live: "#",
-        github: "https://github.com/FreakDevelopers/FreakDevs-Invoice"
+        live: "https://dattakrupajewellers.in/",
+        github: "https://github.com/amrutgawade/dattakrupajewellers"
     },
     {
         id: 4,
-        title: "NOTE-APP",
+        title: "FreakDevs-Invoice",
         category: ["UIUX", "MERN"],
         image: Project4,
-        live: "#",
-        github: "https://github.com/amrutgawade/NOTE-APP"
+        live: "https://invoice.freakdevs.in/",
+        github: "https://github.com/amrutgawade/FreakDevs-Invoice"
     },
     {
         id: 5,
+        title: "NOTE-APP",
+        category: ["MERN"],
+        image: Project5,
+        live: "https://note-app-h6uj.onrender.com",
+        github: "https://github.com/amrutgawade/NOTE-APP"
+    },
+    {
+        id: 6,
         title: "CeramicsHeaven",
         category: ["UIUX", "Springboot"],
-        image: Project5,
+        image: Project6,
         live: "#",
         github: "https://github.com/amrutgawade/ceramicsheaven-client"
     },
     {
-        id: 6,
-        title: "MediSync",
-        category: ["UIUX", "Springboot"],
-        image: Project6,
+        id: 7,
+        title: "MediSync (Hackathon)",
+        category: ["Springboot"],
+        image: Project7,
         live: "#",
         github: "https://github.com/amrutgawade/MediSync"
     },
